ff58c66f361967c598f6514efc879fc24e95bd7d
boiser <hy875631563@outlook.com>
2026-01-20 16:25:22 +0800

    make single thread rpc do the work

--- src/mr/coordinator.go
+++ src/mr/coordinator.go
@@ -1,18 +1,211 @@
 package mr
 
-import "log"
-import "net"
-import "os"
-import "net/rpc"
-import "net/http"
+import (
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"net/rpc"
+	"os"
+	"strconv"
+	"strings"
+)
 
+type Phase int
+
+const (
+	MapPhase Phase = iota
+	ReducePhase
+	AllDone
+)
 
 type Coordinator struct {
 	// Your definitions here.
-
+	TaskPhase Phase
+	NextTaskId int
+	MapTaskChannel chan *Task
+	ReduceTaskChannel chan *Task
+	TaskMap map[int]*Task
+	ReduceNum int
+	MapNum int
 }
 
 // Your code here -- RPC handlers for the worker to call.
+//
+// generate map tasks
+//
+func (c *Coordinator) GenMapTask(files []string) {
+	fmt.Println("begin make map tasks...")
+
+	for _, file := range files {
+		id := c.GenTaskId()
+		input := []string{file}
+		mapTask := Task{
+			TaskId: id,
+			TaskType: MapTask,
+			TaskState: Ready,
+			Files: input,
+			ReduceNum: c.ReduceNum,
+		}
+		fmt.Println("make reduce task, taskid:", id)
+		c.MapTaskChannel <- &mapTask
+	}
+}
+
+//
+// generate reduce tasks
+//
+func (c *Coordinator) GenReduceTask() {
+	fmt.Println("begin make reduce tasks...")
+
+	rn := c.ReduceNum
+
+	dir, _ := os.Getwd()
+	files, err := os.ReadDir(dir)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	for i := 0; i < rn; i++ {
+		id := c.GenTaskId()
+		input := []string{}
+		
+		for _, file := range files {
+			if strings.HasPrefix(file.Name(), "mr-") && strings.HasSuffix(file.Name(), strconv.Itoa(i)) {
+				input = append(input, file.Name())
+			} 
+		}
+
+		reduceTask := Task {
+			TaskId: id,
+			TaskType: ReduceTask,
+			TaskState: Ready,
+			Files: input,
+			ReduceNum: c.ReduceNum,
+			ReduceIdx: id - c.MapNum,
+		}
+		fmt.Println("make reduce task, taskid:", id)
+		c.ReduceTaskChannel <- &reduceTask
+	}
+}
+
+//
+// check if all the map tasks are finished
+//
+func (c *Coordinator) MapTaskDone() bool {
+	mapDoneNum := 0
+	for _, v := range c.TaskMap {
+		if v.TaskType == MapTask && v.TaskState == Finished {
+			mapDoneNum++
+		}
+	}
+	if mapDoneNum == c.MapNum {
+		return true
+	}
+	return false
+}
+
+//
+// check if all the reduce tasks are finished
+//
+func (c *Coordinator) ReduceTaskDone() bool {
+	reduceDoneNum := 0
+	for _, v := range c.TaskMap {
+		if v.TaskType == ReduceTask && v.TaskState == Finished {
+			reduceDoneNum++
+		}
+	}
+	if reduceDoneNum == c.ReduceNum {
+		return true
+	}
+	return false
+}
+
+//
+// switch phase to next
+//
+func (c *Coordinator) NextPhase() {
+	switch c.TaskPhase {
+	case MapPhase: 
+		c.TaskPhase = ReducePhase
+		c.GenReduceTask()
+	case ReducePhase: c.TaskPhase = AllDone
+	}
+}
+
+//
+// RequestTask RPC handler
+// the RPC argument and reply types are defined in rpc.go
+//
+func (c *Coordinator) AssignTask(args *TaskArgs, reply *TaskReply) error {
+	switch c.TaskPhase {
+	case MapPhase: 
+		if len(c.MapTaskChannel) > 0 {
+			taskp := <- c.MapTaskChannel
+			if taskp.TaskState == Ready {
+				reply.Answer = TaskGetted
+				reply.ResTask = *taskp
+				c.TaskMap[taskp.TaskId] = taskp
+				taskp.TaskState = Working
+				fmt.Printf("Task[%d] has been assigned.\n", taskp.TaskId)
+			}
+		} else {
+			reply.Answer = TaskWait
+
+			if c.MapTaskDone() {
+				c.NextPhase()
+			}
+			
+			return nil
+		}
+	case ReducePhase:
+		if len(c.ReduceTaskChannel) > 0 {
+			taskp := <- c.ReduceTaskChannel
+			if taskp.TaskState == Ready {
+				reply.Answer = TaskGetted
+				reply.ResTask = *taskp
+				c.TaskMap[taskp.TaskId] = taskp
+				taskp.TaskState = Working
+				fmt.Printf("Task[%d] has been assigned.\n", taskp.TaskId)
+			}
+		} else {
+			reply.Answer = TaskWait
+
+			if c.ReduceTaskDone() {
+				c.NextPhase()
+			}
+
+			return nil
+		}
+	case AllDone:
+		reply.Answer = TaskFinish
+		fmt.Println("all tasks finished")
+	default:
+		log.Fatalf("undifined phase")
+	}
+	
+	return nil
+}
+
+//
+// when work finish the task, will report to cooridinator
+// modify the task state
+//
+func (c *Coordinator) UpdateTaskState(args *FinishArgs, reply *FinishReply) error {
+	id := args.TaskId
+	taskp := c.TaskMap[id]
+	taskp.TaskState = Finished
+	return nil
+}
+
+//
+// generate taskid for the new task
+//
+func (c *Coordinator) GenTaskId() int {
+	res := c.NextTaskId
+	c.NextTaskId++
+	return res
+}
 
 //
 // an example RPC handler.
@@ -46,12 +239,8 @@ func (c *Coordinator) server() {
 // if the entire job has finished.
 //
 func (c *Coordinator) Done() bool {
-	ret := false
-
 	// Your code here.
-
-
-	return ret
+	return c.TaskPhase == AllDone
 }
 
 //
@@ -60,10 +249,18 @@ func (c *Coordinator) Done() bool {
 // nReduce is the number of reduce tasks to use.
 //
 func MakeCoordinator(files []string, nReduce int) *Coordinator {
-	c := Coordinator{}
-
 	// Your code here.
-
+	c := Coordinator{
+		TaskPhase: MapPhase,
+		NextTaskId: 0,
+		MapTaskChannel: make(chan *Task, len(files)),
+		ReduceTaskChannel: make(chan *Task, nReduce),
+		TaskMap: make(map[int]*Task, len(files) + nReduce),
+		ReduceNum: nReduce,
+		MapNum: len(files),
+	}
+	
+	c.GenMapTask(files)
 
 	c.server()
 	return &c
--- src/mr/worker.go
+++ src/mr/worker.go
@@ -1,10 +1,17 @@
 package mr
 
-import "fmt"
-import "log"
-import "net/rpc"
-import "hash/fnv"
-
+import (
+	"encoding/json"
+	"fmt"
+	"hash/fnv"
+	"io"
+	"log"
+	"net/rpc"
+	"os"
+	"strconv"
+	"time"
+	"sort"
+)
 
 //
 // Map functions return a slice of KeyValue.
@@ -13,6 +20,14 @@ type KeyValue struct {
 	Key   string
 	Value string
 }
+
+// for sorting by key.
+type ByKey []KeyValue
+
+// for sorting by key.
+func (a ByKey) Len() int           { return len(a) }
+func (a ByKey) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a ByKey) Less(i, j int) bool { return a[i].Key < a[j].Key }
 
 //
 // use ihash(key) % NReduce to choose the reduce
@@ -32,10 +47,165 @@ func Worker(mapf func(string, string) []KeyValue,
 	reducef func(string, []string) string) {
 
 	// Your worker implementation here.
+	workerTask := new(Task)
+	alive := true
+	for alive {
+		answer := RequestTask(workerTask)
+		switch answer {
+		case TaskGetted:
+			switch workerTask.TaskType {
+			case MapTask: DoMapTask(workerTask, mapf)
+						RequestFinish(workerTask.TaskId)
+			case ReduceTask: DoReduceTask(workerTask, reducef)
+						RequestFinish(workerTask.TaskId)
+			}
+		case TaskFinish:
+			alive = false
+		case TaskWait:
+			time.Sleep(time.Millisecond * 500)
+		default:
+			log.Fatalf("request task error")
+		}
+	}
+}
 
-	// uncomment to send the Example RPC to the coordinator.
-	// CallExample()
+// 执行 map 任务
+// mapf 是在 main/mrworker.go 创建 worker 时传入的函数指针
+// workerTask 是在 worker 向 coordinator 请求得到的任务
+func DoMapTask(workerTask *Task, mapf func(string, string) []KeyValue) {
+	//
+	// read each input file
+	// pass it to Map,
+	// accumulate the intermediate Map output.
+	//
+	intermediate := []KeyValue{}
+	for _, filename := range workerTask.Files {
+		file, err := os.Open(filename)
+		if err != nil {
+			log.Fatalf("cannot open %v", filename)
+		}
+		content, err := io.ReadAll(file)
+		if err != nil {
+			log.Fatalf("cannot read %v", filename)
+		}
+		file.Close()
+		kva := mapf(filename, string(content))
+		intermediate = append(intermediate, kva...)
+	}
 
+	//
+	// generate intermediate file
+	// file name formation like: mr-X-Y
+	// X is the TaskId, Y is value of ihash(key) % nreduce
+	//
+	rn := workerTask.ReduceNum
+	for i := 0; i < rn; i++ {
+		iname := "mr-" + strconv.Itoa(workerTask.TaskId) + "-" + strconv.Itoa(i)
+		ifile, _ := os.Create(iname)
+		enc := json.NewEncoder(ifile)
+		for _, kv := range intermediate {
+			if ihash(kv.Key) % rn == i {
+				enc.Encode(&kv)
+			}
+		}
+		ifile.Close()
+	}
+}
+
+// 执行 Reduce 任务
+// reducef 是在 main/mrworker.go 创建 worker 时传入的函数指针
+// workerTask 是在 worker 向 coordinator 请求得到的任务
+func DoReduceTask(workerTask *Task, reducef func(string, []string) string) {
+	intermediate := []KeyValue{}
+
+	intermediate = shuffle(workerTask.Files)
+
+	oname := "mr-out-" + strconv.Itoa(workerTask.ReduceIdx)
+	ofile, _ := os.Create(oname)
+
+	//
+	// call Reduce on each distinct key in intermediate[],
+	// and print the result to mr-out-X.
+	//
+	i := 0
+	for i < len(intermediate) {
+		j := i + 1
+		for j < len(intermediate) && intermediate[j].Key == intermediate[i].Key {
+			j++
+		}
+		values := []string{}
+		for k := i; k < j; k++ {
+			values = append(values, intermediate[k].Value)
+		}
+		output := reducef(intermediate[i].Key, values)
+
+		// this is the correct format for each line of Reduce output.
+		fmt.Fprintf(ofile, "%v %v\n", intermediate[i].Key, output)
+
+		i = j
+	}
+
+	ofile.Close()
+}
+
+// 
+// 将多个文件中的 kv 读取并排序
+//
+func shuffle(files []string) []KeyValue {
+	kva := []KeyValue{}
+	for _, filename := range files {
+		file, err := os.Open(filename)
+		if err != nil {
+			log.Fatalf("cannot open %v", filename)
+		}
+		
+		dec := json.NewDecoder(file)
+		for {
+			var kv KeyValue
+
+			err := dec.Decode(&kv)
+			if err != nil {
+				break
+			}
+			kva = append(kva, kv)
+		}
+		file.Close()
+	}
+
+	sort.Sort(ByKey(kva))
+	return kva
+}
+
+//
+// function to make an RPC call to the coordinator for the task
+//
+// I modified the RPC argument and reply types in rpc.go.
+//
+func RequestTask(workerTask *Task) TaskResponseFlag {
+	args := TaskArgs{}
+	reply := TaskReply{}
+
+	ok := call("Coordinator.AssignTask", &args, &reply) // Need coordinator's function
+	if ok {
+		if reply.Answer == TaskGetted {
+			*workerTask = reply.ResTask
+		}
+		return reply.Answer
+	} 
+	return TaskFailed
+}
+
+//
+// function to make an RPC call to the coordinator for the task-finish report
+//
+func RequestFinish(taskId int) {
+	args := FinishArgs{TaskId: taskId}
+	reply := FinishReply{}
+
+	ok := call("Coordinator.UpdateTaskState", &args, &reply)
+	if !ok {
+		log.Fatalf("cannot report task finish")
+	}
 }
 
 //
