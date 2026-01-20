d04c977de8a468e2e55f1ec69590719111c5b0f1
boiser <hy875631563@outlook.com>
2026-01-20 20:10:03 +0800

    restructure and add mutex for multithreading

--- src/mr/coordinator.go
+++ src/mr/coordinator.go
@@ -9,6 +9,7 @@ import (
 	"os"
 	"strconv"
 	"strings"
+	"sync"
 	"time"
 )
 
@@ -24,11 +25,11 @@ type Coordinator struct {
 	// Your definitions here.
 	TaskPhase Phase
 	NextTaskId int
-	MapTaskChannel chan *Task
-	ReduceTaskChannel chan *Task
+	TaskChannel chan *Task
 	TaskMap map[int]*Task
 	ReduceNum int
 	MapNum int
+	Mu sync.Mutex
 }
 
 // Your code here -- RPC handlers for the worker to call.
@@ -60,7 +61,7 @@ func (c *Coordinator) GenMapTask(files []string) {
 			StartTime: time.Now().UnixMilli(),
 		}
 		fmt.Println("make reduce task, taskid:", id)
-		c.MapTaskChannel <- &mapTask
+		c.TaskChannel <- &mapTask
 	}
 }
 
@@ -98,34 +99,33 @@ func (c *Coordinator) GenReduceTask() {
 			StartTime: time.Now().UnixMilli(),
 		}
 		fmt.Println("make reduce task, taskid:", id)
-		c.ReduceTaskChannel <- &reduceTask
+		c.TaskChannel <- &reduceTask
 	}
 }
 
 //
-// check if all the map tasks are finished
+// check if all the tasks are finished
 //
-func (c *Coordinator) MapTaskDone() bool {
-	mapDoneNum := 0
-	for _, v := range c.TaskMap {
-		if v.TaskType == MapTask && v.TaskState == Finished {
-			mapDoneNum++
-		}
-	}
-	return mapDoneNum == c.MapNum
-}
+func (c *Coordinator) TaskDone() bool {
+	DoneNum, TotalNum := 0, 0
+	var TaskType TaskType
 
-//
-// check if all the reduce tasks are finished
-//
-func (c *Coordinator) ReduceTaskDone() bool {
-	reduceDoneNum := 0
+	switch c.TaskPhase {
+	case MapPhase:
+		TotalNum = c.MapNum
+		TaskType = MapTask
+	case ReducePhase:
+		TotalNum = c.ReduceNum
+		TaskType = ReduceTask
+	}
 	for _, v := range c.TaskMap {
-		if v.TaskType == ReduceTask && v.TaskState == Finished {
-			reduceDoneNum++
+		if v.TaskType == TaskType && v.TaskState == Finished {
+			DoneNum++
 		}
 	}
-	return reduceDoneNum == c.ReduceNum
+	
+
+	return DoneNum == TotalNum
 }
 
 //
@@ -145,50 +145,29 @@ func (c *Coordinator) NextPhase() {
 // the RPC argument and reply types are defined in rpc.go
 //
 func (c *Coordinator) AssignTask(args *TaskArgs, reply *TaskReply) error {
-	switch c.TaskPhase {
-	case MapPhase: 
-		if len(c.MapTaskChannel) > 0 {
-			taskp := <- c.MapTaskChannel
-			if taskp.TaskState == Ready {
-				reply.Answer = TaskGetted
-				reply.ResTask = *taskp
-				c.TaskMap[taskp.TaskId] = taskp
-				taskp.TaskState = Working
-				fmt.Printf("Task[%d] has been assigned.\n", taskp.TaskId)
-			}
-		} else {
-			reply.Answer = TaskWait
-
-			if c.MapTaskDone() {
-				c.NextPhase()
-			}
-			
-			return nil
-		}
-	case ReducePhase:
-		if len(c.ReduceTaskChannel) > 0 {
-			taskp := <- c.ReduceTaskChannel
-			if taskp.TaskState == Ready {
-				reply.Answer = TaskGetted
-				reply.ResTask = *taskp
-				c.TaskMap[taskp.TaskId] = taskp
-				taskp.TaskState = Working
-				fmt.Printf("Task[%d] has been assigned.\n", taskp.TaskId)
-			}
-		} else {
-			reply.Answer = TaskWait
-
-			if c.ReduceTaskDone() {
-				c.NextPhase()
-			}
-
-			return nil
-		}
-	case AllDone:
+	if c.TaskPhase == AllDone {
 		reply.Answer = TaskFinish
 		fmt.Println("all tasks finished")
-	default:
-		log.Fatalf("undifined phase")
+		return nil
+	}
+
+	defer c.Mu.Unlock()
+	c.Mu.Lock()
+	if len(c.TaskChannel) > 0 {
+		taskp := <- c.TaskChannel
+		if taskp.TaskState == Ready {
+			reply.Answer = TaskGetted
+			reply.ResTask = *taskp
+			c.TaskMap[taskp.TaskId] = taskp
+			taskp.TaskState = Working
+			fmt.Printf("Task[%d] has been assigned.\n", taskp.TaskId)
+		}
+	} else {
+		reply.Answer = TaskWait
+
+		if c.TaskDone() {
+			c.NextPhase()
+		}
 	}
 	
 	return nil
@@ -245,8 +224,7 @@ func MakeCoordinator(files []string, nReduce int) *Coordinator {
 	c := Coordinator{
 		TaskPhase: MapPhase,
 		NextTaskId: 0,
-		MapTaskChannel: make(chan *Task, len(files)),
-		ReduceTaskChannel: make(chan *Task, nReduce),
+		TaskChannel: make(chan *Task, len(files) + nReduce),
 		TaskMap: make(map[int]*Task, len(files) + nReduce),
 		ReduceNum: nReduce,
 		MapNum: len(files),
