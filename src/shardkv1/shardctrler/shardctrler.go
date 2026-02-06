f26fc37fbe662d1f6d32abc2c07838892622c448
boiser <hy875631563@outlook.com>
2026-02-06 10:52:35 +0800

    update

--- src/shardkv1/shardctrler/shardctrler.go
+++ src/shardkv1/shardctrler/shardctrler.go
@@ -5,32 +5,264 @@ package shardctrler
 //
 
 import (
-	"log"
-	"strconv"
-	"fmt"
+	"encoding/binary"
+	"sync"
+	"sync/atomic"
 	"time"
 
-	"6.5840/kvsrv1"
+	kvsrv "6.5840/kvsrv1"
 	"6.5840/kvsrv1/rpc"
-	"6.5840/kvtest1"
+	kvtest "6.5840/kvtest1"
 	"6.5840/shardkv1/shardcfg"
 	"6.5840/shardkv1/shardgrp"
-	"6.5840/tester1"
+	tester "6.5840/tester1"
 )
 
-var shardConfigName string = "shardconfig_curr"
-var shardConfigNextName string = "shardconfig_next"
-var controllerLockName string = "controller_mutex"
+// key name for shard configuration
+const config_key_name string = "shard_config"
+const next_config_key_name string = "next_shard_config"
+const controller_lock_name string = "controller_lock"
+const lease_duration time.Duration = 2 * time.Second
 
 // ShardCtrler for the controller and kv clerk.
 type ShardCtrler struct {
 	clnt *tester.Clnt
 	kvtest.IKVClerk
 
-	killed int32 // set by Kill()
+	killed       int32 // set by Kill()
+	leases       bool
+	leaseVersion rpc.Tversion
 
-	// Your data here.
-	leaseExpiry time.Time
+	grpsClerks map[tester.Tgid]*shardgrp.Clerk // one clerk for each shard group
+}
+
+// ensureGrpClerksFor makes sure sck.grpsClerks has a clerk for every group in cfg.
+func (sck *ShardCtrler) ensureGrpClerksFor(cfg *shardcfg.ShardConfig) {
+	for gid, servers := range cfg.Groups {
+		if _, ok := sck.grpsClerks[gid]; ok {
+			continue
+		}
+		if len(servers) == 0 {
+			sck.grpsClerks[gid] = nil
+		} else {
+			sck.grpsClerks[gid] = shardgrp.MakeClerk(sck.clnt, servers)
+		}
+	}
+}
+
+// finishReconfig migrates shards from old -> new (idempotent via Num) and then
+// sets the controller's current config to new by CAS on config_key_name using curVersion.
+// return rpc.OK if succeed; rpc.Errversion if superseded.
+func (sck *ShardCtrler) finishReconfig(old *shardcfg.ShardConfig, new *shardcfg.ShardConfig, curVersion rpc.Tversion) rpc.Err {
+	// Create clerks for all groups mentioned in either config (needed after a crash).
+	sck.ensureGrpClerksFor(old)
+	sck.ensureGrpClerksFor(new)
+
+	// Migrate shards that change ownership.
+	var wg sync.WaitGroup
+	for i := 0; i < shardcfg.NShards; i++ {
+		shid := shardcfg.Tshid(i)
+		oldGrpId := old.Shards[shid]
+		newGrpId := new.Shards[shid]
+		if oldGrpId == newGrpId {
+			continue
+		}
+		wg.Add(1)
+		go func(shid shardcfg.Tshid, oldGrpId, newGrpId tester.Tgid) {
+			defer wg.Done()
+			// 1) Freeze on old owner using old.Num.
+			var shardBytes []byte
+
+			for {
+				if !sck.isHoldingLock() {
+					return
+				}
+				b, err := sck.grpsClerks[oldGrpId].FreezeShard(shid, old.Num)
+				if err == rpc.OK {
+					shardBytes = b
+					break
+				}
+				if err == rpc.ErrVersion {
+					return
+				}
+				time.Sleep(100 * time.Millisecond)
+			}
+
+			// 2) Install on new owner with new.Num.
+			for {
+				if !sck.isHoldingLock() {
+					return
+				}
+				err := sck.grpsClerks[newGrpId].InstallShard(shid, shardBytes, new.Num)
+				if err == rpc.OK {
+					break
+				}
+				if err == rpc.ErrVersion {
+					return
+				}
+				time.Sleep(50 * time.Millisecond)
+			}
+
+			// 3) Delete on old owner with new.Num.
+			for {
+				if !sck.isHoldingLock() {
+					return
+				}
+				err := sck.grpsClerks[oldGrpId].DeleteShard(shid, new.Num)
+				if err == rpc.OK {
+					break
+				}
+				if err == rpc.ErrVersion {
+					return
+				}
+				time.Sleep(100 * time.Millisecond)
+			}
+		}(shid, oldGrpId, newGrpId)
+	}
+	wg.Wait()
+
+	// Drop clerks for groups that no longer exist in the new config.
+	for gid := range old.Groups {
+		if _, ok := new.Groups[gid]; !ok {
+			delete(sck.grpsClerks, gid)
+		}
+	}
+
+	// Atomically advance current config from old -> new.
+	for {
+		err := sck.IKVClerk.Put(config_key_name, new.String(), curVersion)
+		if err == rpc.OK {
+			break
+		}
+		if err == rpc.ErrVersion {
+			return rpc.ErrVersion
+		}
+		if err == rpc.ErrMaybe {
+			_, v, e := sck.IKVClerk.Get(config_key_name)
+			if e == rpc.OK && v == curVersion+1 {
+				break
+			}
+		}
+		time.Sleep(100 * time.Millisecond)
+	}
+
+	return rpc.OK
+}
+
+func (sck *ShardCtrler) acquireLock() {
+	for {
+		val, ver, _ := sck.IKVClerk.Get(controller_lock_name)
+		lease_time := unserializeTime(val)
+		now := time.Now()
+		new_lease_time := serializeTime(now.Add(lease_duration))
+		if now.After(lease_time) {
+			err := sck.IKVClerk.Put(controller_lock_name, new_lease_time, ver)
+			if err == rpc.OK {
+				// Acquire lock successfully.
+				sck.leaseVersion = ver + 1
+				return
+			}
+			if err == rpc.ErrMaybe {
+				val2, ver2, _ := sck.IKVClerk.Get(controller_lock_name)
+				if ver2 == ver+1 && val2 == new_lease_time {
+					// Acquire lock successfully.
+					sck.leaseVersion = ver + 1
+					return
+				}
+			}
+		}
+
+		time.Sleep(50 * time.Millisecond)
+	}
+}
+
+func (sck *ShardCtrler) isHoldingLock() bool {
+	if !sck.leases {
+		return true
+	}
+
+	val, version, _ := sck.IKVClerk.Get(controller_lock_name)
+	if version != sck.leaseVersion || time.Now().After(unserializeTime(val)) {
+		return false
+	}
+	return true
+}
+
+func (sck *ShardCtrler) renew() bool {
+	val, version, _ := sck.IKVClerk.Get(controller_lock_name)
+
+	if version != sck.leaseVersion || time.Now().After(unserializeTime(val)) {
+		// Not a lease holder.
+		return false
+	}
+
+	for {
+		leaseTime := serializeTime(time.Now().Add(lease_duration))
+		err := sck.IKVClerk.Put(controller_lock_name, leaseTime, version)
+		if err == rpc.OK {
+			break
+		}
+		if err == rpc.ErrMaybe {
+			val2, ver2, _ := sck.IKVClerk.Get(controller_lock_name)
+			if ver2 == version+1 && val2 == leaseTime {
+				break
+			}
+		}
+	}
+
+	sck.leaseVersion = version + 1
+	return true
+}
+
+func (sck *ShardCtrler) startAutoRenew() (stop func()) {
+	done := make(chan struct{})
+	go func() {
+		t := time.NewTicker(lease_duration / 2)
+		defer t.Stop()
+		for {
+			select {
+			case <-t.C:
+				if !sck.renew() {
+					return
+				}
+			case <-done:
+				return
+			}
+		}
+	}()
+	return func() { close(done) }
+}
+
+func (sck *ShardCtrler) releaseLock() {
+	for {
+		_, ver, _ := sck.IKVClerk.Get(controller_lock_name)
+		now := serializeTime(time.Now())
+		err := sck.IKVClerk.Put(controller_lock_name, now, ver)
+		if err == rpc.OK {
+			return
+		}
+		if err == rpc.ErrMaybe {
+			val2, ver2, _ := sck.IKVClerk.Get(controller_lock_name)
+			if ver2 == ver+1 && val2 == now {
+				return
+			}
+		}
+	}
+}
+
+func serializeTime(t time.Time) string {
+	buf := make([]byte, binary.MaxVarintLen64)
+	n := binary.PutVarint(buf, t.UnixMilli())
+	return string(buf[:n])
+}
+
+func unserializeTime(t string) time.Time {
+	buf := []byte(t)
+	ms, n := binary.Varint(buf)
+	if n <= 0 {
+		return time.UnixMilli(0)
+	}
+	return time.UnixMilli(ms)
 }
 
 // Make a ShardCltler, which stores its state in a kvsrv.
@@ -38,266 +270,145 @@ func MakeShardCtrler(clnt *tester.Clnt) *ShardCtrler {
 	sck := &ShardCtrler{clnt: clnt}
 	srv := tester.ServerName(tester.GRP0, 0)
 	sck.IKVClerk = kvsrv.MakeClerk(clnt, srv)
-	// Your code here.
+	sck.grpsClerks = make(map[tester.Tgid]*shardgrp.Clerk)
 	return sck
-}
-
-func serializeLease(t time.Time) string {
-	return fmt.Sprintf("%d", t.UnixMilli())
-}
-
-func parseLease(val string) time.Time {
-	if val == "" {
-		return time.UnixMilli(0)
-	}
-	ms, err := strconv.ParseInt(val, 10, 64)
-	if err != nil {
-		return time.UnixMilli(0)
-	}
-	return time.UnixMilli(ms)
-}
-
-// Use new config to apply migration among servers
-func (sck *ShardCtrler) ApplyMigration(curr, next *shardcfg.ShardConfig) bool {
-	// check all shards
-	for i := range len(next.Shards) {
-		srcGid := curr.Shards[i]
-		dstGid := next.Shards[i]
-
-		// if valid srcGid
-		if srcGid != dstGid {
-			var state []byte
-			var srcServers, dstServers []string
-			var srcCk, dstCk *shardgrp.Clerk
-			var err rpc.Err
-			
-			if srcGid != 0 {
-				srcServers = curr.Groups[srcGid]
-				srcCk = shardgrp.MakeClerk(sck.clnt, srcServers)
-
-				for {
-					if time.Now().After(sck.leaseExpiry) {
-						return false
-					}
-					state, err = srcCk.FreezeShard(shardcfg.Tshid(i), curr.Num)
-					if err == rpc.OK {
-						break
-					}
-					if err == rpc.ErrWrongGroup {
-						state = nil
-						break
-					}
-					time.Sleep(100 * time.Millisecond)
-					// log.Printf("ShardCtrler: fail to freeze shard %v in group %v. Error: %v\n", i, srcGid, err)
-				}
-			}
-
-			if dstGid != 0 && state != nil {
-				dstServers = next.Groups[dstGid]
-				dstCk = shardgrp.MakeClerk(sck.clnt, dstServers)
-
-				for {
-					if time.Now().After(sck.leaseExpiry) {
-						return false
-					}
-					err = dstCk.InstallShard(shardcfg.Tshid(i), state, next.Num)
-					if err == rpc.OK {
-						break
-					}
-					time.Sleep(100 * time.Millisecond)
-					// log.Printf("ShardCtrler: fail to install shard %v in group %v. Error: %v\n", i, dstGid, err)
-				}
-			}
-
-			if srcGid != 0 && state != nil {
-				for {
-					if time.Now().After(sck.leaseExpiry) {
-						return false
-					}
-					err = srcCk.DeleteShard(shardcfg.Tshid(i), curr.Num)
-					if err == rpc.OK {
-						break
-					}
-					time.Sleep(100 * time.Millisecond)
-					// log.Printf("ShardCtrler: fail to install shard %v in group %v. Error: %v\n", i, srcGid, err)
-				}
-			}
-		}
-	}
-	return true
-}
-
-func (sck *ShardCtrler) AcquireLock() {
-	for {
-		val, ver, err := sck.IKVClerk.Get(controllerLockName)
-		// 如果 key 不存在或为空，expiry 为 0 时间，必然小于 Now
-		expiry := parseLease(val)
-		now := time.Now()
-
-		// 如果租约已过期，尝试获取锁
-		if now.After(expiry) {
-			// 设置 1.0 秒的租约时间
-			newExpiry := time.UnixMilli(now.Add(1000 * time.Millisecond).UnixMilli())
-			newVal := serializeLease(newExpiry)
-			
-			// 尝试 CAS 更新
-			err = sck.IKVClerk.Put(controllerLockName, newVal, ver)
-			if err == rpc.OK {
-				sck.leaseExpiry = newExpiry
-				return
-			}
-			// 如果 Put 失败（版本不匹配），说明锁被别人更新了，继续循环
-		} else {
-			// 锁未过期，等待重试
-			time.Sleep(50 * time.Millisecond)
-		}
-	}
-}
-
-func (sck *ShardCtrler) ReleaseLock() {
-	for {
-		val, ver, err := sck.IKVClerk.Get(controllerLockName) 
-		if err != rpc.OK && err != rpc.ErrNoKey {
-			return // 无法访问存储，放弃释放
-		}
-
-		// 检查锁是否还是自己的
-		currentExpiry := parseLease(val)
-		if currentExpiry.UnixMilli() != sck.leaseExpiry.UnixMilli() {
-			return // 锁已经不是自己的了（可能过期被别人抢了），不需要释放
-		}
-
-		// 尝试置空释放
-		// 注意：这里用 time.UnixMilli(0) 表示 0 时间，即立即过期
-		emptyVal := serializeLease(time.UnixMilli(0))
-		err = sck.IKVClerk.Put(controllerLockName, emptyVal, ver)
-		if err == rpc.OK {
-			return
-		}
-		// 如果 Put 失败，可能是发生了变化，循环重试检查
-		time.Sleep(10 * time.Millisecond)
-	}
 }
 
 // The tester calls InitController() before starting a new
 // controller. In part A, this method doesn't need to do anything. In
-// B and C, this method implements recovery.
-func (sck *ShardCtrler) InitController() {
-	// Acquire lock
-	sck.AcquireLock()
-	defer sck.ReleaseLock()
+// B and C, this method implements recovery (part B) and uses a lock
+// to become leader (part C). InitController should return
+// rpc.ErrVersion when another controller supersedes it (e.g., when
+// this controller is partitioned during recovery); this happens only
+// in Part C. Otherwise, it returns rpc.OK.
+func (sck *ShardCtrler) InitController() rpc.Err {
+	sck.leases = true
 
-	// 1. Check current config
-	currStr, currVer, err := sck.IKVClerk.Get(shardConfigName)
-	if err != rpc.OK {
-		// If accessing kvsrv fails, we can't do much. 
-		// But usually InitController relies on kvsrv being up.
-		return
-	}
-	currCfg := shardcfg.FromString(currStr)
+	sck.acquireLock()
+	defer sck.releaseLock()
 
-	// 2. Check next config
-	nextStr, _, err := sck.IKVClerk.Get(shardConfigNextName)
-	if err != rpc.OK || nextStr == "" {
-		return
-	}
-	nextCfg := shardcfg.FromString(nextStr)
+	curVal, curVer, _ := sck.IKVClerk.Get(config_key_name)
+	nextVal, _, _ := sck.IKVClerk.Get(next_config_key_name)
+	curCfg := shardcfg.FromString(curVal)
+	nextCfg := shardcfg.FromString(nextVal)
 
-	// 3. Keep going if next > current
-	if nextCfg.Num > currCfg.Num {
-		if !sck.ApplyMigration(currCfg, nextCfg) {
-			return
-		}
-		// Update current to match next
-		// Note: We use currVer from the read. If it changed in between,  
-		// it means another controller might have finished it.
-		sck.IKVClerk.Put(shardConfigName, nextCfg.String(), currVer)
+	if nextCfg.Num > curCfg.Num {
+		// A previous controller started a reconfig but didn't finish. Complete it now.
+		return sck.finishReconfig(curCfg, nextCfg, curVer)
 	}
+
+	// Nothing to recover; either equal or next is stale.
+	return rpc.OK
+}
+
+// The tester calls ExitController to exit a controller. In part B and
+// C, release lock.
+func (sck *ShardCtrler) ExitController() {
 }
 
 // Called once by the tester to supply the first configuration.  You
 // can marshal ShardConfig into a string using shardcfg.String(), and
 // then Put it in the kvsrv for the controller at version 0.  You can
-// pick the key to name the configuration.  The initial configuration
-// lists shardgrp shardcfg.Gid1 for all shards.
+// pick the key to name the configuration.
 func (sck *ShardCtrler) InitConfig(cfg *shardcfg.ShardConfig) {
-	// Your code here
-	sck.IKVClerk.Put(shardConfigName, cfg.String(), 0)
-	sck.IKVClerk.Put(shardConfigNextName, cfg.String(), 0)
-	sck.IKVClerk.Put(controllerLockName, serializeLease(time.Now()), 0)
+	sck.IKVClerk.Put(config_key_name, cfg.String(), 0)
+	sck.IKVClerk.Put(next_config_key_name, cfg.String(), 0)
+	sck.IKVClerk.Put(controller_lock_name, serializeTime(time.Now()), 0)
+	sck.leases = false
 
-	
+	// Initialize the group clerks for each group
+	for gid, servers := range cfg.Groups {
+		if len(servers) == 0 {
+			sck.grpsClerks[gid] = nil // no servers for this shard
+		} else {
+			sck.grpsClerks[gid] = shardgrp.MakeClerk(sck.clnt, servers)
+		}
+	}
 }
 
 // Called by the tester to ask the controller to change the
-// configuration from the current one to new.  While the controller
-// changes the configuration it may be superseded by another
-// controller.
-func (sck *ShardCtrler) ChangeConfigTo(new *shardcfg.ShardConfig) {
+// configuration from the current one to new. It should return
+// rpc.ErrVersion if this controller is superseded by another
+// controller, as in part C.  In all other cases, it should return
+// rpc.OK.
+func (sck *ShardCtrler) ChangeConfigTo(new *shardcfg.ShardConfig) rpc.Err {
+	// Get old config.
+	var old *shardcfg.ShardConfig
+	for {
+		val, _, err := sck.IKVClerk.Get(config_key_name)
+
+		if err == rpc.OK {
+			old = shardcfg.FromString(val)
+			break
+		}
+		time.Sleep(50 * time.Millisecond)
+	}
+
+	if new.Num != old.Num+1 {
+		return rpc.ErrVersion
+	}
 
 	// Acquire lock
-	sck.AcquireLock()
-	defer sck.ReleaseLock()
+	sck.acquireLock()
+	stop := sck.startAutoRenew()
+	defer func() { stop(); sck.releaseLock() }()
 
-	// get cfg
-	str, version, err := sck.IKVClerk.Get(shardConfigName)
-	if err != rpc.OK {
-		log.Printf("ShardCtrler: fail to read config.")
-	}
-	cfg := shardcfg.FromString(str)
-	
-	// check num
-	if new.Num <= cfg.Num {
-		log.Printf("new config's num is less than cfg\n")
-		return
-	}
-
-	_, nextVer, _ := sck.IKVClerk.Get(shardConfigNextName)
-	err = sck.IKVClerk.Put(shardConfigNextName, new.String(), nextVer)
-	for err != rpc.OK {
-		if time.Now().After(sck.leaseExpiry) {
-			return
-		}
-		// Retry saving intent
-		_, nextVer, _ = sck.IKVClerk.Get(shardConfigNextName)
-		err = sck.IKVClerk.Put(shardConfigNextName, new.String(), nextVer)
-		time.Sleep(10 * time.Millisecond)
-	}
-
-	if !sck.ApplyMigration(cfg, new) {
-		return
-	}
-
-	// update new config
+	// write next_config_key_name
+	new_string := new.String()
 	for {
-		if time.Now().After(sck.leaseExpiry) {
-			return
+		if !sck.isHoldingLock() {
+			return rpc.ErrVersion
 		}
-		err = sck.IKVClerk.Put(shardConfigName, new.String(), version)
+		err := sck.IKVClerk.Put(next_config_key_name, new_string, rpc.Tversion(old.Num))
 		if err == rpc.OK {
 			break
 		}
-		log.Printf("ShardCtrler: fail to update config., err: %v", err)
-		str, ver, getErr := sck.IKVClerk.Get(shardConfigName)
-		if getErr == rpc.OK {
-			c := shardcfg.FromString(str)
-			if c.Num >= new.Num {
-				break
-			}
-			version = ver
+		if err == rpc.ErrVersion {
+			return rpc.ErrVersion
 		}
-		time.Sleep(10 * time.Millisecond)
+		if err == rpc.ErrMaybe {
+			val, v, gerr := sck.IKVClerk.Get(next_config_key_name)
+			if gerr == rpc.OK {
+				if v == rpc.Tversion(old.Num)+1 && new_string == val {
+					break
+				}
+				if v > rpc.Tversion(old.Num)+1 || new_string != val {
+					return rpc.ErrVersion
+				}
+			}
+		}
+		time.Sleep(50 * time.Millisecond)
 	}
+
+	// Ensure we have clerks for any new groups before migration (finishReconfig also ensures).
+	if !sck.isHoldingLock() {
+		return rpc.ErrVersion
+	}
+	sck.ensureGrpClerksFor(new)
+
+	return sck.finishReconfig(old, new, rpc.Tversion(old.Num))
 }
 
+// Tester "kills" shardctrler by calling Kill().  For your
+// convenience, we also supply isKilled() method to test killed in
+// loops.
+func (sck *ShardCtrler) Kill() {
+	atomic.StoreInt32(&sck.killed, 1)
+}
 
-// Return the current configuration
+func (sck *ShardCtrler) isKilled() bool {
+	z := atomic.LoadInt32(&sck.killed)
+	return z == 1
+}
+
+// Return the current configuration and its version number
 func (sck *ShardCtrler) Query() *shardcfg.ShardConfig {
-	// Your code here.
-	str, _, err := sck.IKVClerk.Get(shardConfigName)
-	if err != rpc.OK {
-		log.Fatalf("ShardCtrler: fail to read config.")
+	for {
+		val, _, err := sck.IKVClerk.Get(config_key_name)
+		if err == rpc.OK {
+			return shardcfg.FromString(val)
+		}
+		// Add delay to avoid overwhelming the server
+		time.Sleep(50 * time.Millisecond)
 	}
-	return shardcfg.FromString(str)
 }
-
